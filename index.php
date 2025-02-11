04536e566939f0975307f547376e52f8fa1aa494
MatheusSouzaTech <matheusestudotech@gmail.com>
2025-02-10 21:47:51 -0300

    Criando layout das paginas iniciasi

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Login</title>
+</head>
+<body>
+
+    <div class="conteiner">
+
+        <h1>Login</h1>
+
+
+        <form method="post">
+
+            <label for="email">Login</label>
+            <input type="text" name="email" id="email" placeholder="email" required>
+
+            <label for="senha">Senha</label>
+            <input type="text" name="senha" id="senha" placeholder="senha" required>
+
+            <button type="submit" name="logar">Entrar</button>
+            <button type="submit" name="cadastrar">Cadastrar</button>
+
+        </form>
+
+
+
+
+    </div>
+    
+</body>
+</html>
